0a405ab4d13891449774d20008be8e19cfe5854e
liako <108364802+mantashashvili@users.noreply.github.com>
2022-10-01 08:49:21 +0400

    Add files via upload

---
+++ challange3/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>challange 3</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/main.css">
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<script src="https://cdn.tailwindcss.com"></script>
+<link rel="stylesheet" href="/public/css/form.css">
+</head>
+<body class="bg-teal-600">
+
+<style>
+        
+      </style>
+   </head>
+   <body>
+      <div class="form">
+         
+         <form style="width:100%;" method="post" action="/challange3/search.php">
+         <label for="">Enter country name</label>
+            <input type="text" class="text" placeholder="country" name="country" value=""/>
+            <input type="submit" value="Submit" class="submit" name="submit"/>
+         </form>
+      </div>
+
+<br></br>
+<p class="text-3xl text-white"> searched history </p>
+<br>
+<a href="history.php" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-5 border border-blue-500 hover:border-transparent rounded">history</a>
+
+</body>
+</html>
+
+
+
